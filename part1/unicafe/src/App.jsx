0d7401ccc733279046e85adec2c11a70528f1d21
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-02-14 16:12:37 -0500

    WIP unicafe

--- part1/unicafe/src/App.jsx
+++ part1/unicafe/src/App.jsx
@@ -1,35 +1,60 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+  const [bad, setBad] = useState(0);
+  let all = good + neutral + bad;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div>
+      <h1>Give Feedback</h1>
+      <button onClick={() => setGood(good + 1)}>good</button>
+      <button onClick={() => setNeutral(neutral + 1)}>neutral</button>
+      <button onClick={() => setBad(bad + 1)}>bad</button>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+    </div>
+  );
+};
 
-export default App
+export default App;
+
+const Statistics = ({ good, bad, neutral, all }) => {
+  let positive = (6 / all) * 100;
+
+  const average = function (good, neutral = 0, bad = -1) {
+    return (good + neutral + bad) / 3;
+  };
+  if (all === 0) {
+    return <p>No Feedback given</p>;
+  }
+  return (
+    <div>
+      <h1>Statistics</h1>
+        <StatisticLine text={"Good"} value={good} />
+        <StatisticLine text={"Neutral"} value={neutral} />
+        <StatisticLine text={"Bad"} value={bad} />
+        <StatisticLine text={"All"} value={all} />
+        <StatisticLine text={"Average"} value={(good + neutral + bad) / 3} />
+        <StatisticLine
+          text={"Positive"}
+          value={`${positive === Infinity ? 0 : positive}%`}
+        />
+    
+    </div>
+  );
+};
+
+const StatisticLine = ({ text, value }) => {
+  return (
+     <table>
+        <tbody>
+        <tr>
+          <td style={{ width: "4rem" }}>{text}</td>
+          <td>{value}</td>
+        </tr>
+        </tbody>
+      </table>
+  );
+};

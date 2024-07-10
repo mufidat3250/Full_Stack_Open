fc271d20e3c3b3fa5530379ab23318b58475dc31
Wahab MMufidat <wahabmufidat1919@gmail.com>
2024-07-10 13:28:07 -0400

    code refractoring

--- part12/part12-containers-applications/todo-app/todo-backend/mongo/models/Todo.js
+++ part12/part12-containers-applications/todo-app/todo-backend/mongo/models/Todo.js
@@ -1,4 +1,12 @@
+require('dotenv').config()
 const mongoose = require('mongoose')
+
+const url = process.env.MONGO_URL
+mongoose.connect(url, { useNewUrlParser: true , useUnifiedTopology: true}).then((res)=> {
+  console.log('database connected successful')
+}).catch((err)=> {
+  console.log('something went wrong while connecting to database')
+})
 
 const todoSchema = new mongoose.Schema({
   text: String,
